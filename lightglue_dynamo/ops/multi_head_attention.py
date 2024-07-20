e3528917f298327874e9fc4529bbb0452cdc7b67
fabio-sim <54187389+fabio-sim@users.noreply.github.com>
2024-07-20 22:07:03 +0700

    fix: BC torch version < 2.4

--- lightglue_dynamo/ops/multi_head_attention.py
+++ lightglue_dynamo/ops/multi_head_attention.py
@@ -13,11 +13,11 @@ def multi_head_attention(q: torch.Tensor, k: torch.Tensor, v: torch.Tensor, num_
     return F.scaled_dot_product_attention(q, k, v).transpose(1, 2).reshape((b, n, d))
 
 
-fused_multi_head_attention = torch.library.custom_op(CUSTOM_OP_NAME, mutates_args=())(multi_head_attention)
+fused_multi_head_attention = None
 
 
 def multi_head_attention_dispatch(q: torch.Tensor, k: torch.Tensor, v: torch.Tensor, num_heads: int) -> torch.Tensor:
-    if FUSE_MULTI_HEAD_ATTENTION:
+    if FUSE_MULTI_HEAD_ATTENTION and fused_multi_head_attention is not None:
         return fused_multi_head_attention(q, k, v, num_heads)
     else:
         return multi_head_attention(q, k, v, num_heads)
@@ -29,6 +29,7 @@ def symbolic_multi_head_attention(g, q, k, v, num_heads_i):
 
 
 def use_fused_multi_head_attention():
-    global FUSE_MULTI_HEAD_ATTENTION  # noqa: PLW0603
+    global FUSE_MULTI_HEAD_ATTENTION, fused_multi_head_attention  # noqa: PLW0603
     FUSE_MULTI_HEAD_ATTENTION = True
+    fused_multi_head_attention = torch.library.custom_op(CUSTOM_OP_NAME, mutates_args=())(multi_head_attention)
     torch.onnx.register_custom_op_symbolic(CUSTOM_OP_NAME, symbolic_multi_head_attention, 9)
